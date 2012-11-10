1d291228fee3a8ff8040fe85d8fad7520ba67d33
Giedrius Rimkus <info@giedriusr.lt>
2012-11-10 22:57:47 +0100

    removing twitter message

--- includes/follow-us.php
+++ includes/follow-us.php
@@ -1,4 +1,3 @@
 <div id="follow-us">
- <blockquote class="twitter-tweet tw-align-center"><p>Why there are no rally managers in general? Isn't WRC that much popular after all? I think it is in the same level with F-1</p>&mdash; Dirt Rally (@dirtrally) <a href="https://twitter.com/dirtrally/status/225995192925306880" data-datetime="2012-07-19T16:47:07+00:00">July 19, 2012</a></blockquote>
-  <script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
 </div>
